35cb43f92f8315c5e74d7d4aefb04cb345421858
reyvbg-hub <reyvbg@gmail.com>
2026-08-13 18:15:37 +0530

    chore: update service worker configuration
    Update domain and zoneId to point to the new tracking endpoint.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11567065
+    "domain": "5gvci.com",
+    "zoneId": 11568483
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
